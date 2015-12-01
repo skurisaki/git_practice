65db2a88186569ec96c45d7f649d72f7b3f8990e
skurisaki <skurisaki@crooz.co.jp>
2015-12-02 01:29:13 +0900

    secound

--- plactice1_5.java
+++ plactice1_5.java
@@ -1,7 +1,7 @@
 /**
- * 足し算の例
+ * Practice 1_5
  *
- * @since 2015/11/30
+ * @since 2015/12/01
  * @author sotakurisaki
  */
 public class Main{
@@ -11,5 +11,11 @@ public class Main{
     int c;
     c = a * b;
     System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    
+    boolean result = true;
+    char c = '駆';
+    float f = 3.14;
+    long a = 314159265853979L
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。"
   }
 }

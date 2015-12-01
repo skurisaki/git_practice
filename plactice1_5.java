cec7ac731885ae3bd5a011ac603f6bb0a2e1b032
skurisaki <skurisaki@crooz.co.jp>
2015-12-02 00:38:28 +0900

    plactice1_5

---
+++ plactice1_5.java
@@ -0,0 +1,15 @@
+/**
+ * ‘«‚µZ‚Ì—á
+ *
+ * @since 2015/11/30
+ * @author sotakurisaki
+ */
+public class Main{
+  public static void main(String[] args){
+    int a = 3;
+    int b = 5;
+    int c;
+    c = a * b;
+    System.out.println("c•" + a + "‰¡•" + b + "‚Ì’·•ûŒ`‚Ì–ÊÏ‚ÍA" + c);
+  }
+}

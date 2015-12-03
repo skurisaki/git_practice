ab8daab14b637bd1294c71b3f0f16d34043314bf
skurisaki <skurisaki@crooz.co.jp>
2015-12-04 00:15:36 +0900

    first

---
+++ plactice3_8_5.java
@@ -0,0 +1,21 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.print("[ƒƒjƒ…[]@‚PFŒŸõ‚QF“o˜^‚RFíœ‚SF•ÏX„");
+		int selected = new java.util.Scanner(System.in).nextInt();
+
+		switch (selected){
+			case 1:
+				System.out.println("ŒŸõ‚µ‚Ü‚·");
+				break;
+			case 2:
+				System.out.println("“o˜^‚µ‚Ü‚·");
+				break;
+			case 3:
+				System.out.println("íœ‚µ‚Ü‚·");
+				break;
+			case 4:
+				System.out.println("•ÏX‚µ‚Ü‚·");
+				break;
+		}
+	}
+}

0264b3f90b99a6ec1808c5720ed86167dcb42aad
skurisaki <skurisaki@crooz.co.jp>
2015-12-18 23:55:39 +0900

    initial commit

---
+++ paiza/turime.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int a = new java.util.Scanner(System.in).nextInt();
+        for(int i=0;i<a;i++){
+            System.out.print("Ann");
+        }
+    }
+}

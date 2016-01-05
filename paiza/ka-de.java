9e3de357debd13e3f07db2f03ddad9732395c6e8
skurisaki <skurisaki@crooz.co.jp>
2016-01-05 21:01:53 +0900

    firstcommit

---
+++ paiza/ka-de.java
@@ -0,0 +1,18 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int a = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int x = Integer.parseInt(line);
+        
+        for(int i=1;i<=x;i++){
+            a = a*i;
+        }
+        System.out.println(a);
+    }
+}
